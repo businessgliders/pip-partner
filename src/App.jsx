17a0189bf1d45ed9ce3dbd1d458b089af7b853cb
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-03-20 03:52:51 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Hire from './pages/Hire';
+import FrontAdmin from './pages/FrontAdmin';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
