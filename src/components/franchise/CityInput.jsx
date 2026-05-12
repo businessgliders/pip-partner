e81d488fc58e60ff9a5fca8c780e5a44f04bdde4
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-05-12 16:46:52 +0000

    File changes

--- src/components/franchise/CityInput.jsx
+++ src/components/franchise/CityInput.jsx
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 
 const CITIES_BY_PROVINCE = {
-  Ontario: ["Vaughan, ON", "Oakville, ON", "Burlington, ON", "Mississauga, ON", "Markham, ON", "Ottawa, ON", "London, ON", "Hamilton, ON", "Kitchener, ON"],
-  "British Columbia": ["Vancouver, BC", "Burnaby, BC", "Richmond, BC", "Victoria, BC", "Surrey, BC", "Kelowna, BC"],
   Alberta: ["Calgary, AB", "Edmonton, AB", "Airdrie, AB", "Red Deer, AB", "Sherwood Park, AB"],
+  "British Columbia": ["Vancouver, BC", "Burnaby, BC", "Richmond, BC", "Victoria, BC", "Surrey, BC", "Kelowna, BC"],
+  Manitoba: ["Winnipeg, MB", "Brandon, MB", "Steinbach, MB"],
+  "New Brunswick": ["Moncton, NB", "Saint John, NB", "Fredericton, NB"],
+  "Newfoundland and Labrador": ["St. John's, NL", "Mount Pearl, NL", "Corner Brook, NL"],
+  "Nova Scotia": ["Halifax, NS", "Dartmouth, NS", "Sydney, NS"],
+  "Northwest Territories": ["Yellowknife, NT"],
+  Nunavut: ["Iqaluit, NU"],
+  Ontario: ["Vaughan, ON", "Oakville, ON", "Burlington, ON", "Mississauga, ON", "Markham, ON", "Ottawa, ON", "London, ON", "Hamilton, ON", "Kitchener, ON"],
+  "Prince Edward Island": ["Charlottetown, PE", "Summerside, PE"],
+  Quebec: ["Montreal, QC", "Quebec City, QC", "Laval, QC", "Gatineau, QC", "Longueuil, QC"],
+  Saskatchewan: ["Saskatoon, SK", "Regina, SK", "Prince Albert, SK"],
+  Yukon: ["Whitehorse, YT"],
 };
 
-const DEFAULT_CITIES = ["Vaughan, ON", "Vancouver, BC", "Calgary, AB", "Oakville, ON"];
+const DEFAULT_CITIES = ["Toronto, ON", "Vancouver, BC", "Calgary, AB", "Montreal, QC", "Ottawa, ON", "Edmonton, AB", "Winnipeg, MB", "Halifax, NS"];
 
 export default function CityInput({ value, onChange, province, className }) {
   const cities = province && CITIES_BY_PROVINCE[province] ? CITIES_BY_PROVINCE[province] : DEFAULT_CITIES;
